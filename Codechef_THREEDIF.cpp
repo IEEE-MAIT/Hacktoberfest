a8edb231e809bb88ffa74738cadfbcd2f0e34fb8
Akshat mangal <64827980+iamakshat01@users.noreply.github.com>
2020-10-01 12:48:26 +0530

    Create Codechef_THREEDIF.cpp

---
+++ Codechef_THREEDIF.cpp
@@ -0,0 +1,28 @@
+//Problem Link: https://www.codechef.com/problems/THREEDIF
+#include<bits/stdc++.h>
+# define ll long long 
+
+using namespace std;
+const ll m =1e9+7;
+int main()
+{
+  ll t;
+  cin>>t;
+  while(t--)
+  {
+    vector<ll>v(3);
+    for(int i=0;i<3;i++)
+        {
+            cin>>v[i];
+            //v[i]=(v[i])%m;
+        }
+        sort(v.begin(),v.end());
+        ll r=1;
+        for(int i=0;i<3;i++)
+        {
+           r=(r*((v[i]-i)%m))%m;
+        }
+       
+        cout<<r<<endl;
+  }
+}
