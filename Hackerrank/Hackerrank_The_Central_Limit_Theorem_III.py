5f5cbe8f4810a639577c73ad7d71ac91eb241ebc
Ujwal Kandi <51212164+UjwalKandi@users.noreply.github.com>
2020-10-02 21:04:03 +0530

    Create Hackerrank_The_Central_Limit_Theorem_III.py

---
+++ Hackerrank/Hackerrank_The_Central_Limit_Theorem_III.py
@@ -0,0 +1,10 @@
+
+from math import sqrt
+
+n = int(input())
+mean = int(input())
+std_dev = int(input())
+interval = float(input())
+z = float(input())
+print(round(mean - (std_dev / sqrt(n)) * z, 2))
+print(round(mean + (std_dev / sqrt(n)) * z, 2))
