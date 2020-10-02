f149f9ef091e7298e0380d0c32eafc52f10752cb
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-10-02 13:58:18 +0530

    Leetcode

---
+++ Leetcode/Leetcode_8.cpp
@@ -0,0 +1,19 @@
+/*
+ * Intersection of Two Linked Lists
+ * Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+ * Output: Reference of the node with value = 8
+ * Input Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect). From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+ */
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *A = headA;
+        ListNode *B = headB;
+        while (A != B) {
+            A = (A != NULL) ? A->next : headB;
+            B = (B != NULL) ? B->next : headA;
+        }
+        return A;
+    }
+};
