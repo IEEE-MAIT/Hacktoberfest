79f6ca36d2e24d8ec459c86e5e04f1b5861c40ed
joaldotavares <joaldo.junior@ucsal.edu.br>
2020-10-04 14:32:15 -0300

    Codeforces 459B resolved

--- Codeforces/Codeforces_261_459B.cpp
+++ Codeforces/Codeforces_261_459B.cpp
@@ -1,12 +1,29 @@
 #include<bits/stdc++.h>
+#define ll long long
 using namespace std;
-main()
+int main()
 {
-	long long i,j,n,a[200040];
-	for(cin>>n;j++<n;)cin>>a[j];
-	sort(a,a+n+1);
-	for(;a[++i]==a[i+1];);
-	for(;a[--j]==a[j-1];);
-	if(i>j)cout<<0<<' '<<n*(n-1)/2;
-	else cout<<a[n]-a[1]<<' '<<i*(n-j+1);
+   ll n;
+   cin>>n;
+   ll a[n],i,max=-1,min=INT_MAX,max_cnt=0,min_cnt=0;
+    for(i=0;i<n;i++)
+     {
+         cin>>a[i];
+         if(a[i]>max)
+            max=a[i],max_cnt=1;
+         else if(a[i]==max)
+            max_cnt++;
+
+         if(a[i]<min)
+            min=a[i],min_cnt=1;
+         else if(a[i]==min)
+            min_cnt++;
+     }
+     //cout<<max<< " " <<min<<endl;
+     if(max==min)
+        cout<<"0 "<<(ll)( (min_cnt-1)*(min_cnt)/2 );
+    else
+     cout<<max-min<<" "<<(ll)(min_cnt*max_cnt);
+
+return 0;
 }
